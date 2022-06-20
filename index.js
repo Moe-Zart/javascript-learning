17b4aedbe64e2919ee9442624dccafd7b9ad833d
Moe-Zart <mohamedzreika03@outlook.com>
2022-06-20 12:49:45 +1000

    bsaic, functions and arrays

---
+++ index.js
@@ -0,0 +1,78 @@
+/*Function
+function sentenceBlanks(hardness, wasa, front1, front2, front3) {
+  var sentence =
+    "On a scale from 1-10, " +
+    hardness +
+    " " +
+    front1 +
+    " " +
+    wasa +
+    " " +
+    3 +
+    ", " +
+    hardness +
+    " " +
+    front2 +
+    " " +
+    wasa +
+    " " +
+    5 +
+    ", and " +
+    hardness +
+    " " +
+    front3 +
+    " " +
+    wasa +
+    " " +
+    7 +
+    ".";
+  return sentence;
+}
+console.log(
+  sentenceBlanks("the difficulty of", "was a", "HTML", "CSS", "Javascript")
+);
+*/
+
+
+/*Array
+var array = ["Moe", 19, "Bachelor of Business"];
+array.shift(); //removed "Moe" and made 19 index 0
+array.unshift("Moe"); //added "Moe" and made it index 0
+array[2] = "Bachelor of Software Engineering"; //replaced the previous array[2] "bachelor of business".
+var sentence =
+  "My name is " +
+  array[0] +
+  " and I am " +
+  array[1] +
+  " years old, doing a " +
+  array[2] +
+  ".";
+array.push("yay"); //added an index 3 to the array as "yay"
+array.pop(); // removed index 3 "yay".
+console.log(sentence + array[3]);
+*/
+
+
+/*function with no arguments
+function myFunction(){
+console.log("nice "+2+" see u.");
+}
+myFunction();
+*/
+
+
+/*Function with arguments
+function myFunction(a,b,c){
+  console.log(a + b + c);
+}
+myFunction(1,2,3);
+*/
+
+
+/*Function with return
+function plusEight(x){
+var result=x+8;
+return result;
+}
+console.log(plusEight(2));
+*/
